0b5834ec9f3431931d6686022cd597b6589f4fda
stanislav <79920025475@yandex.ru>
2022-12-06 21:57:32 +0500

    palindrom

---
+++ palindrom/Program.cs
@@ -0,0 +1,13 @@
+﻿// task19
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int newNumber = 0;
+for (int i = n.Length - 1; i >= 0; i--)
+{
+    newNumber = newNumber + Convert.ToInt32(int.Parse(n[i].ToString())*Math.Pow(10,i));
+} 
+Console.WriteLine(newNumber);
+if (newNumber == int.Parse(n))
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
