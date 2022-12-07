880e80552c806450ab63ad37040ce0cafc7f11da
stanislav <79920025475@yandex.ru>
2022-12-07 18:37:31 +0500

    task37

---
+++ task37/Program.cs
@@ -0,0 +1,23 @@
+﻿// task37
+
+
+void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array [i] = new Random().Next (-1,11); //[-1,10]
+}
+
+void ReleaseArray (int[] array)
+{
+    for (int i = 0; i < array.Length /2 + array.Length % 2; i++)
+    Console.Write($"{array[i] * array[array.Length - i - 1]}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+InputArray (array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray (array);
+
