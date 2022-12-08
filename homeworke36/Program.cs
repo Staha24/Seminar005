e4557a65d926cf75f21c4f35e81f7d127f0629cf
stanislav <79920025475@yandex.ru>
2022-12-08 12:51:33 +0500

    homeworke36

--- homeworke36/Program.cs
+++ homeworke36/Program.cs
@@ -15,9 +15,9 @@ for (int z = 0; z < numbers.Length; z+=2)
 
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i+=2)
+    for(int i = 0; i < numbers.Length; i++)
         {
-            numbers[i] = new Random().Next(1,10);
+            numbers[i] = new Random().Next(1,100);
         }
 }
 void PrintArray(int[] numbers)
